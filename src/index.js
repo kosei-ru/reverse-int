5bef5ecccae9cb1396248ceed15983e583e55302
kosei-ru <kosei.ru@gmail.com>
2023-02-11 13:39:30 +0300

    done

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
 module.exports = function reverse (n) {
+  let arr1 = Math.abs(n).toString().split("");
+  let arr2 = [];
+
+  for (let i = arr1.length - 1; i >= 0; i--) arr2.push(arr1[i]);
+  return Number(arr2.join(""));
   
+  //return Number(Math.abs(n).toString().split("").reverse().join(""));
 }
